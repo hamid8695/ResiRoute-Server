2d2ad359836e8b79a0d0f8a4375ce67f41e62f51
unknown <hamidazad020@gmail.com>
2024-01-25 23:30:17 +0600

    updated

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const helmet = require("helmet");
 const cors = require('cors');
 const corsOptions ={
     origin:'http://localhost:5173', 
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:false,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
