6b3fdb405f9c614d4cacd08a38f8b68d71ed1907
unknown <hamidazad020@gmail.com>
2024-01-25 23:23:33 +0600

    updated

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ const createError = require('http-errors')
 const bodyParser = require('body-parser')
 const app = express();
 const helmet = require("helmet");
-const cors = require("cors");
+// const cors = require("cors");
 
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
+// app.use(cors({
+//     origin: 'http://localhost:5173'
+// }));
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 const userRouter = require('./routers/user.Route');
 const adminRouter = require('./routers/admin.Route');
 const residentRouter = require('./routers/resident.Route');
