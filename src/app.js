ca10bea2f8fab5f29bd8529bf0620abd702d79f9
unknown <hamidazad020@gmail.com>
2024-01-30 11:27:36 +0600

    updated code

--- src/app.js
+++ src/app.js
@@ -14,10 +14,11 @@ app.use(helmet());
 app.use(morgan("dev"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: false,
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: ['http://localhost:5173'],
+//     credentials: false,
+// }));
 
 
 // all routes
