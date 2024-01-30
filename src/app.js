3901c7676308e599fc05bccd485df876ddd6086b
unknown <hamidazad020@gmail.com>
2024-01-30 11:18:03 +0600

    mupated

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
     origin: ['http://localhost:5173'],
-    credentials: true,
+    credentials: false,
 }));
 
 
